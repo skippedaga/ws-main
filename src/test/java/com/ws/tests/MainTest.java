f58a81dcb0599195663d0878fd20047cf8b7bf5e
a.glushchenko <a.glushchenko@ukrcard.com.ua>
2015-06-03 12:25:23 +0300

    Change-Id: Ief7924214f4cfe3467a9ea63285bb634419f3f29

--- src/test/java/com/ws/tests/MainTest.java
+++ src/test/java/com/ws/tests/MainTest.java
@@ -26,7 +26,7 @@ public class MainTest extends CamelTestSupport {
 
 	@EndpointInject(uri = "mock:result")
 	protected MockEndpoint resultEndpoint;
-	
+
 	@Before
 	public void SetUp() throws Exception {
 		new MainServer(); // Starting Jetty SOAP server
@@ -35,11 +35,11 @@ public class MainTest extends CamelTestSupport {
 	@Test
 	public void firstStep() throws IOException_Exception,
 			SOAPException_Exception {
-		
+
 		MyActionServerService srv = new MyActionServerService();
 		MyActionServer serv = srv.getMyActionServerPort();
 
-		// Put msg to SOAP serv
+		// Put i msg to SOAP serv
 		for (int i = 1; i <= 2; i++) {
 
 			MyActionBody actionBody = new MyActionBody();
@@ -64,41 +64,43 @@ public class MainTest extends CamelTestSupport {
 	}
 
 	protected RouteBuilder createRouteBuilder() throws Exception {
-		
+
 		return new RouteBuilder() {
 			public void configure() throws Exception {
-				
+
 				from("activemq:queue:Income")
-				.unmarshal()
-					.soapjaxb("com.ws.client")
-					.split()
-					.body()
-					.convertBodyTo(MyActionMethod.class)
-					.marshal()
-					.soapjaxb("com.ws.client")
-					.to("activemq:queue:Outcome")
-				.log(LoggingLevel.INFO,
+						.unmarshal()
+						.soapjaxb("com.ws.client")
+						.split()
+						.body()
+						.convertBodyTo(MyActionMethod.class)
+						.marshal()
+						.soapjaxb("com.ws.client")
+						.to("activemq:queue:Outcome")
+						.log(LoggingLevel.INFO,
 								"message N:"
-										+ simple("${in.header.head}").getText()
-										+ " file:"
-										+ simple("${in.header.filename}")
-												.getText()
-										+ " "
-										+ simple("${in.body}").getText()).to("mock:result");
-				
+									+ simple("${in.header.head}").getText()
+									+ " file:"
+									+ simple("${in.header.filename}")
+											.getText() + " "
+									+ simple("${in.body}").getText())
+						.to("mock:result");
+
 				from("activemq:queue:Outcome").to("").to("mock:result");
 			}
 		};
 	}
 
 	@BeforeClass
-	public static  void init(){ 
-	    System.setProperty( "com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "true");
+	public static void init() {
+		System.setProperty(
+				"com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "true");
 	}
-	
+
 	@AfterClass
-	public static void revert(){ 
-	    System.getProperties().remove("com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize");
-	}    
+	public static void revert() {
+		System.getProperties().remove(
+				"com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize");
+	}
 
 }
